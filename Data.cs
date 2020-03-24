6556e52664b25a9a768e8edca4ec807cbcd0b788
Synful <47508058+Vaxure1337@users.noreply.github.com>
2020-03-24 13:04:32 -0700

    Create Data.cs

---
+++ Data.cs
@@ -0,0 +1,13 @@
+namespace _9HitsData
+{
+    public class Data
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Joined { get; set; }
+        public string Token { get; set; }
+        public string Funds { get; set; }
+        public string Points { get; set; }
+        public string Membership { get; set; }
+    }
+}
